41b38e2fb9dfd322cab8e67ce15d3de03b8fbc9a
Rishi Goyal <rishigoyal230@gmail.com>
2022-04-13 17:26:22 +0530

    added saveTask

--- main.js
+++ main.js
@@ -3,15 +3,32 @@ var fs = require("fs");
 var app = express();
 
 app.use(express.static("todo"));
+app.use(express.json());
 
 app.get("/todo", function (req, res) {
-	console.log(req.body)
+	console.log(req.body);
 	fs.readFile("./db.txt", "utf-8", function (err, data) {
 		res.end(data);
 	});
 });
 
-
+app.post("/save", function (req, res) {
+	fs.readFile("./db.txt", "utf-8", function (err, data) {
+		var todos = [];
+		if (data.length > 0) {
+			todos = JSON.parse(data);
+		}
+		todos.push(req.body);
+		console.log(req.body);
+		fs.writeFile("./db.txt", JSON.stringify(todos), function (err, data) {
+			if (err) {
+				res.end("error", err);
+			} else {
+				res.send("saved");
+			}
+		});
+	});
+});
 
 app.listen(3000, function () {
 	console.log("listening on 3000");
--- todo/script.js
+++ todo/script.js
@@ -1,98 +1,97 @@
-var taskContainer = document.getElementById('tasks');
-var input = document.getElementById('todoContainer');
-var saveButton = document.getElementById('button');
+var taskContainer = document.getElementById("tasks");
+var input = document.getElementById("todoContainer");
+var saveButton = document.getElementById("button");
 
-saveButton.addEventListener('click', saveTask);
+saveButton.addEventListener("click", saveTask);
 
 function onLoad() {
-    input.value = '';
+	input.value = "";
+	getAllTasks(function (tasks) {
+		tasks.forEach(function (task) {
+			createListItem(task);
+		});
+	});
 }
 
 onLoad();
 
 function createListItem(task) {
-    var list = document.createElement('div');
-    list.setAttribute('class', 'list-item');
+	var list = document.createElement("div");
+	list.setAttribute("class", "list-item");
 
-    var span = document.createElement('span');
-    span.setAttribute('class', 'span');
-    span.innerHTML = task.todo;
+	var span = document.createElement("span");
+	span.setAttribute("class", "span");
+	span.innerHTML = task.todo;
 
-    var controls = document.createElement('div');
-    controls.setAttribute('class', 'controls');
+	var controls = document.createElement("div");
+	controls.setAttribute("class", "controls");
 
-    var deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('class', 'controlBtn');
-    deleteBtn.innerHTML = 'Delete';
-    deleteBtn.addEventListener('click', deleteTask(task));
+	var deleteBtn = document.createElement("button");
+	deleteBtn.setAttribute("class", "controlBtn");
+	deleteBtn.innerHTML = "Delete";
+	deleteBtn.addEventListener("click", deleteTask(task));
 
-    var editBtn = document.createElement('button');
-    editBtn.setAttribute('class', 'controlBtn');
-    editBtn.innerHTML = 'Edit';
-    editBtn.addEventListener('click', editTask(task));
+	var editBtn = document.createElement("button");
+	editBtn.setAttribute("class", "controlBtn");
+	editBtn.innerHTML = "Edit";
+	editBtn.addEventListener("click", editTask(task));
 
-    var isMarked = document.createElement('input');
-    isMarked.setAttribute('type', 'checkbox');
-    if (task.isDone) {
-        isMarked.checked = true;
-        span.style.textDecoration = 'line-through';
-    }
+	var isMarked = document.createElement("input");
+	isMarked.setAttribute("type", "checkbox");
+	if (task.isDone) {
+		isMarked.checked = true;
+		span.style.textDecoration = "line-through";
+	}
 
-    isMarked.onchange = changeCheckStatus(task);
+	isMarked.onchange = changeCheckStatus(task);
 
-    controls.appendChild(isMarked);
-    controls.appendChild(editBtn);
-    controls.appendChild(deleteBtn);
-    list.appendChild(span);
-    list.appendChild(controls);
-    taskContainer.appendChild(list);
+	controls.appendChild(isMarked);
+	controls.appendChild(editBtn);
+	controls.appendChild(deleteBtn);
+	list.appendChild(span);
+	list.appendChild(controls);
+	taskContainer.appendChild(list);
 }
 
 function saveTask() {
-    var taskContent = input.value;
-    if (taskContent.length > 0) {
-        var data = {
-            todo: taskContent,
-            isDone: false,
-            id: Date.now()
-        }
+	var taskContent = input.value;
+	if (taskContent.length > 0) {
+		var data = {
+			todo: taskContent,
+			isDone: false,
+			id: Date.now(),
+		};
 
-        var body = JSON.stringify(data);
+		var body = JSON.stringify(data);
 
-        var request = new XMLHttpRequest();
-        request.open('POST', '/save');
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(body);
+		var request = new XMLHttpRequest();
+		request.open("POST", "/save");
+		request.setRequestHeader("Content-Type", "application/json");
+		console.log(body);
+		request.send(body);
 
-        request.addEventListener('load', function () {
-            createListItem(data);
-            input.value = '';
-        });
-    }
-    else {
-        alert('Please enter a task');
-    }
+		request.addEventListener("load", function () {
+			createListItem(data);
+			input.value = "";
+		});
+	} else {
+		alert("Please enter a task");
+	}
 }
 
 function getAllTasks(onResponse) {
-    var request = new XMLHttpRequest();
-    request.open('GET', '/todo');
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send();
+	var request = new XMLHttpRequest();
+	request.open("GET", "/todo");
+	request.setRequestHeader("Content-Type", "application/json");
+	request.send();
 
-    request.addEventListener('load', () => {
-        onResponse(JSON.parse(request.responseText));
-    });
+	request.addEventListener("load", () => {
+		onResponse(JSON.parse(request.responseText));
+	});
 }
 
-function deleteTask() {
+function deleteTask() {}
 
-}
+function editTask() {}
 
-function editTask() {
-
-}
-
-function changeCheckStatus() {
-    
-}
+function changeCheckStatus() {}
